14ad58d51eb7f5c733261b06e8211c9b042aad00
Sebastian <129107648+sebandrien@users.noreply.github.com>
2023-04-08 22:30:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
+import warnings #Import libraries 
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
+from xgboost import XGBRegressor
+from sklearn.metrics import r2_score
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+
+warnings.filterwarnings('ignore')
+
+df = pd.read_csv("diamonds.csv") #Read in "diamonds" to df
+
+df.head() #Display "df's" head
+
+df.tail() #Display "df's" tail
+
+df.info() #Display "df's" info
+
+from sklearn.preprocessing import LabelEncoder
+
+def labelencoder(df):
+    for c in df.columns:
+        if df[c].dtype=='object': 
+            df[c] = df[c].fillna('N')
+            lbl = LabelEncoder()
+            lbl.fit(list(df[c].values))
+            df[c] = lbl.transform(df[c].values)
+    return df
+  
+df=labelencoder(df.copy())
+display(df)
+
+plt.figure(dpi=1000)
+plt.figure(figsize=(20, 10))
+sns.heatmap(df.corr(), annot=True, cmap="YlGnBu")
